edf15fe2065574fb6263b3eb6521cb0a5dd3ecbf
Dmytrotarn <112310528+Dmytrotarn@users.noreply.github.com>
2024-05-17 22:32:03 +0300

    Some work

---
+++ src/js/1-timer.js
@@ -0,0 +1,9 @@
+import flatpickr from "flatpickr"
+import "flatpickr/dist/flatpickr.min.css"
+
+flatpickr("#datetime-picker", {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+});
